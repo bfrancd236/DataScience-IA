498e1ef23dcef801dd7dc78b56361ee682f6202c
bfrancd236 <franciscogedesnet@hotmail.com>
2021-08-12 18:35:19 -0300

    biblioteca Tabu em R

---
+++ Tabu-R/TabuExecucao.R
@@ -0,0 +1,11 @@
+library(tabuSearch)
+
+z = tabuSearch(size = 12, iters = 1000, objFunc = compras, 
+               listSize = 9, nRestarts = 10, repeatAll = 1, verbose = T)
+
+plot(z)
+
+summary(z)
+
+
+summary(z, verbose = TRUE)
